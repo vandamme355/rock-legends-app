7c8c4429b619e5495c47c28e5ac187484f1944aa
vandamme355 <98933357+vandamme355@users.noreply.github.com>
2025-07-19 00:35:50 -0400

    Update EntradaTest.java

--- src/main/java/EntradaTest.java
+++ src/main/java/EntradaTest.java
@@ -1,1 +1,28 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EntradaTest {
+
+    @Test
+    public void testTipoEntradaPorDefecto() {
+        Entrada entrada = new Entrada();
+        assertEquals("VIP", entrada.getTipo());
+    }
+
+    @Test
+    public void testPrecioEntradaPorDefecto() {
+        Entrada entrada = new Entrada();
+        assertEquals(120000, entrada.getPrecio());
+    }
+
+    @Test
+    public void testMostrarInfoNoLanzaExcepcion() {
+        Entrada entrada = new Entrada();
+        try {
+            entrada.mostrarInfo();
+        } catch (Exception e) {
+            fail("mostrarInfo() lanzó una excepción: " + e.getMessage());
+        }
+    }
+}
 
